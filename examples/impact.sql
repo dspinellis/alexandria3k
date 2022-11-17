808af49ef25d0aeff25be0d68597c10a781ce14e
Diomidis Spinellis <dds@aueb.gr>
2022-11-17 12:25:05 +0200

    Handle DOIs as case insensitive

--- examples/impact.sql
+++ examples/impact.sql
@@ -1,7 +1,8 @@
 ATTACH 'impact_data.db' AS impact_data;
 
 CREATE TABLE works_issn AS
-  SELECT doi, Coalesce(issn_print, issn_electronic) AS issn, published_year
+  SELECT Lower(doi) AS doi,
+    Coalesce(issn_print, issn_electronic) AS issn, published_year
   FROM impact_data.works
   WHERE issn is not null;
 
@@ -11,10 +12,11 @@ CREATE TABLE citations AS
   SELECT cited_work.issn, COUNT(*) AS citations_number
   FROM impact_data.work_references
   INNER JOIN works_issn AS published_work
-    ON work_references.work_doi = published_work.doi
+    ON Lower(work_references.work_doi) = published_work.doi
   INNER JOIN works_issn AS cited_work
-    ON work_references.doi = cited_work.doi
+    ON Lower(work_references.doi) = cited_work.doi
   WHERE published_work.published_year = 2021
+    AND cited_work.published_year BETWEEN 2019 AND 2020
   GROUP BY cited_work.issn;
 
 CREATE TABLE publications AS
@@ -23,8 +25,9 @@ CREATE TABLE publications AS
   GROUP BY issn;
 
 CREATE TABLE impact_factor AS
-  SELECT publications.issn,
-    Coalesce(citations_number, 0) / publications_number AS impact_factor
+  SELECT publications.issn, citations_number, publications_number,
+    Cast(Coalesce(citations_number, 0) AS FLOAT) / publications_number
+      AS impact_factor
   FROM publications
   LEFT JOIN citations ON citations.issn = publications.issn
   WHERE publications_number > 0;
