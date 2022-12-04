9347eb2b6878aa4006ef9b4fc96d4114db157a78
Diomidis Spinellis <dds@aueb.gr>
2022-12-04 16:24:34 +0200

    Add script to update usage reference info

---
+++ src/alexandria3k/update-usage.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Alexandria3k Crossref bibliographic metadata processing
+# Copyright (C) 2022  Diomidis Spinellis
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Update usage info in the README.md file
+#
+
+set -e
+
+TEMP=$$.md
+README=../../README.md
+
+sed -n '1,/<!-- CLI start -->/p' $README >$TEMP
+echo '```' >>$TEMP
+python ./alexandria3k.py -h >>$TEMP
+echo '```' >>$TEMP
+sed -n '/<!-- CLI end -->/,$p' $README >>$TEMP
+mv $TEMP $README
