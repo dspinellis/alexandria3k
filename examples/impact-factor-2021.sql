e3a2b5b7387f03ecf261e18d667f388166b5c1eb
Diomidis Spinellis <dds@aueb.gr>
2022-12-10 10:13:01 +0200

    Update impact factor calculation

--- examples/impact-factor-2021.sql
+++ examples/impact-factor-2021.sql
@@ -1,32 +1,39 @@
 -- Calculate the 2021 journal impact factor
 
-ATTACH 'impact_data.db' AS impact_data;
-
-CREATE TABLE works_issn AS
-  SELECT doi AS doi,
+.print CREATE TABLE works_issn
+CREATE TABLE IF NOT EXISTS works_issn AS
+  SELECT id, doi,
     Coalesce(issn_print, issn_electronic) AS issn, published_year
-  FROM impact_data.works
-  WHERE issn is not null;
+  FROM works
+  WHERE issn is not null
+    -- To avoid news articles, book reviews, etc. include works longer than
+    -- two pages
+    AND Substr(page, Instr(page, "-") + 1)
+      - Substr(page, 0, Instr(page, "-")) > 1;
 
-CREATE INDEX works_issn_doi_idx ON works_issn(doi);
+.print CREATE INDEX EXISTS works_issn_doi_idx
+CREATE INDEX IF NOT EXISTS works_issn_doi_idx ON works_issn(doi);
 
-CREATE TABLE citations AS
+.print CREATE TABLE citations
+CREATE TABLE IF NOT EXISTS citations AS
   SELECT cited_work.issn, COUNT(*) AS citations_number
-  FROM impact_data.work_references
+  FROM work_references
   INNER JOIN works_issn AS published_work
-    ON work_references.work_doi = published_work.doi
+    ON work_references.work_id = published_work.id
   INNER JOIN works_issn AS cited_work
     ON work_references.doi = cited_work.doi
   WHERE published_work.published_year = 2021
     AND cited_work.published_year BETWEEN 2019 AND 2020
   GROUP BY cited_work.issn;
 
-CREATE TABLE publications AS
+.print CREATE TABLE publications
+CREATE TABLE IF NOT EXISTS publications AS
   SELECT issn, COUNT(*) AS publications_number FROM works_issn
   WHERE published_year BETWEEN 2019 AND 2020
   GROUP BY issn;
 
-CREATE TABLE impact_factor AS
+.print CREATE TABLE impact_factor
+CREATE TABLE IF NOT EXISTS impact_factor AS
   SELECT Replace(publications.issn, '-', '') AS issn,
     citations_number, publications_number,
     Cast(Coalesce(citations_number, 0) AS FLOAT) / publications_number
@@ -35,11 +42,9 @@ CREATE TABLE impact_factor AS
   LEFT JOIN citations ON citations.issn = publications.issn
   WHERE publications_number > 0;
 
-UPDATE Journal_data SET issn_print=Replace(issn_print, "-", "");
-UPDATE Journal_data SET issn_eprint=Replace(issn_eprint, "-", "");
-
-CREATE INDEX journal_names_issn_print_idx ON journal_names(issn_print);
-CREATE INDEX journal_names_issn_eprint_idx ON journal_names(issn_eprint);
+.print create journal name indexes
+CREATE INDEX IF NOT EXISTS journal_names_issn_print_idx ON journal_names(issn_print);
+CREATE INDEX IF NOT EXISTS journal_names_issn_eprint_idx ON journal_names(issn_eprint);
 
 SELECT issn, title, impact_factor
   FROM impact_factor
