a252aceff896f48e15508541cab7a68267c40e4d
Diomidis Spinellis <dds@aueb.gr>
2022-12-11 21:01:47 +0200

    Add data download code

--- examples/research-synthesis/research-synthesis.sh
+++ examples/research-synthesis/research-synthesis.sh
@@ -3,5 +3,10 @@
 # Output types of research synthesis studies over the years
 #
 
+# Download, if needed the Crossref data through the DOI torrent link
+if ! [ -d 'April 2022 Public Data File from Crossref' ] ; then
+  aria2c https://doi.org/10.13003/83b2gq
+fi
+
 alexandria3k --crossref-directory 'April 2022 Public Data File from Crossref' \
   -query-file research-synthesis.sql
