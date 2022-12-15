b0a0c66acc17977e31ca35ce114b925252d75370
Diomidis Spinellis <dds@aueb.gr>
2022-12-15 14:47:52 +0200

    Create a graph of the Crossref database

---
+++ examples/graph/create-graph.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# Create a graph of interconnected indexed nodes of the Crossref database
+# Omit leaf text elements
+#
+
+CROSSREF_DIR="${CROSSREF_DIR:-'April 2022 Public Data File from Crossref'}"
+DB_FILE="${DB_FILE:-graph.db}"
+TIME="${TIME:-time}"
+SQL_SCRIPT=$(dirname "$0")/index-normalize.sql
+
+# Download, if needed, the Crossref data through the DOI torrent link
+if ! [ -d "$CROSSREF_DIR" ] ; then
+  aria2c https://doi.org/10.13003/83b2gq
+fi
+
+# Populate the database
+$TIME alexandria3k --crossref-directory "$CROSSREF_DIR" \
+ --populate "$DB_FILE" \
+  --debug progress \
+  --columns works.id \
+  works.doi \
+  works.published_year \
+  work_references.work_id \
+  work_references.doi \
+  work_funders.id \
+  work_funders.work_id \
+  work_funders.doi \
+  funder_awards.funder_id \
+  funder_awards.name \
+  author_affiliations.author_id \
+  author_affiliations.name \
+  work_subjects.work_id \
+  work_subjects.name \
+  work_authors.id \
+  work_authors.work_id \
+  work_authors.orcid
+
+# Normalize tables and index
+$TIME sqlite3 "$DB_FILE" <"$SQL_SCRIPT"
---
+++ examples/graph/index-normalize.sql
@@ -0,0 +1,95 @@
+-- Normalize and index the graph database
+
+.print CREATE INDEX works_doi_idx
+CREATE INDEX works_doi_idx ON works(doi);
+.print CREATE INDEX works_id_idx
+CREATE INDEX works_id_idx ON works(id);
+.print CREATE INDEX work_references_doi_idx
+CREATE INDEX work_references_doi_idx ON work_references(doi);
+.print CREATE INDEX work_references_work_id_idx
+CREATE INDEX work_references_work_id_idx ON work_references(work_id);
+.print CREATE INDEX work_funders_id_idx
+CREATE INDEX work_funders_id_idx ON work_funders(id);
+.print CREATE INDEX work_funders_work_id_idx
+CREATE INDEX work_funders_work_id_idx ON work_funders(work_id);
+.print CREATE INDEX work_funders_doi_idx
+CREATE INDEX work_funders_doi_idx ON work_funders(doi);
+.print CREATE INDEX funder_awards_funder_id_idx
+CREATE INDEX funder_awards_funder_id_idx ON funder_awards(funder_id);
+.print CREATE INDEX author_affiliations_author_id_idx
+CREATE INDEX author_affiliations_author_id_idx
+  ON author_affiliations(author_id);
+.print CREATE INDEX work_subjects_work_id_idx
+CREATE INDEX work_subjects_work_id_idx ON work_subjects(work_id);
+.print CREATE INDEX work_authors_work_id_idx
+CREATE INDEX work_authors_work_id_idx ON work_authors(work_id);
+.print CREATE INDEX work_authors_id_idx
+CREATE INDEX work_authors_id_idx ON work_authors(id);
+.print CREATE INDEX work_authors_orcid_idx
+CREATE INDEX work_authors_orcid_idx ON work_authors(orcid);
+
+-- Create affiliation_names id-name table and authors_affiliations,
+-- affiliations_works many-to-many tables
+
+DROP TABLE IF EXISTS affiliation_names;
+
+.print CREATE TABLE affiliation_names
+CREATE TABLE affiliation_names AS
+  SELECT row_number() OVER (ORDER BY '') AS id, name
+  FROM (SELECT DISTINCT name FROM author_affiliations);
+
+.print CREATE INDEX affiliation_names_id_idx
+CREATE INDEX affiliation_names_id_idx ON affiliation_names(id);
+
+DROP TABLE IF EXISTS authors_affiliations;
+.print CREATE TABLE authors_affiliations
+CREATE TABLE authors_affiliations AS
+  SELECT affiliation_names.id AS affiliation_id,
+    author_affiliations.author_id
+    FROM affiliation_names INNER JOIN author_affiliations
+      ON affiliation_names.name = author_affiliations.name;
+
+.print CREATE INDEX authors_affiliations_affiliation_id_idx
+CREATE INDEX authors_affiliations_affiliation_id_idx
+  ON authors_affiliations(affiliation_id);
+.print CREATE INDEX authors_affiliations_author_id_idx
+CREATE INDEX authors_affiliations_author_id_idx
+  ON authors_affiliations(author_id);
+
+DROP TABLE IF EXISTS affiliations_works;
+.print CREATE TABLE affiliations_works
+CREATE TABLE affiliations_works AS
+  SELECT DISTINCT affiliation_id, work_authors.work_id
+    FROM authors_affiliations
+    LEFT JOIN work_authors ON authors_affiliations.author_id = work_authors.id;
+
+.print CREATE INDEX affiliations_works_affiliation_id_idx
+CREATE INDEX affiliations_works_affiliation_id_idx
+  ON affiliations_works(affiliation_id);
+
+.print CREATE INDEX affiliations_works_work_id_idx
+CREATE INDEX affiliations_works_work_id_idx
+  ON affiliations_works(work_id);
+
+-- Create subject_names id-name table and works_subjects many-to-many table
+
+DROP TABLE IF EXISTS subject_names;
+.print CREATE TABLE subject_names
+CREATE TABLE subject_names AS
+  SELECT row_number() OVER (ORDER BY '') AS id, name
+    FROM (SELECT DISTINCT name FROM work_subjects);
+
+.print CREATE INDEX subject_names_id_idx
+CREATE INDEX subject_names_id_idx ON subject_names(id);
+
+DROP TABLE IF EXISTS works_subjects;
+.print CREATE TABLE works_subjects
+CREATE TABLE works_subjects AS
+  SELECT subject_names.id AS subject_id, work_id
+    FROM subject_names
+    INNER JOIN work_subjects ON subject_names.name = work_subjects.name;
+
+.print CREATE INDEX works_subjects_subject_id_idx
+CREATE INDEX works_subjects_subject_id_idx ON works_subjects(subject_id);
+.print CREATE INDEX works_subjects_work_id_idx
+CREATE INDEX works_subjects_work_id_idx ON works_subjects(work_id);
