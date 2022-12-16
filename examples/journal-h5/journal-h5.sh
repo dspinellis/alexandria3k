2c41d7b7e8f5d12a40c2bf9160331c81e43190f7
Diomidis Spinellis <dds@aueb.gr>
2022-12-16 19:47:59 +0200

    Parameterize script

--- examples/journal-h5/journal-h5.sh
+++ examples/journal-h5/journal-h5.sh
@@ -3,20 +3,25 @@
 # Calculate the h5-index of journals
 #
 
+CROSSREF_DIR="${CROSSREF_DIR:-'April 2022 Public Data File from Crossref'}"
+DB_FILE="${DB_FILE:-5y.db}"
+TIME="${TIME:-time}"
+SQL_SCRIPT=$(dirname "$0")/journal-h5.sql
+
 # Download, if needed the Crossref data through the DOI torrent link
-if ! [ -d 'April 2022 Public Data File from Crossref' ] ; then
+if ! [ -d "$CROSSREF_DIR" ] ; then
   aria2c https://doi.org/10.13003/83b2gq
 fi
 
 # Populate database with DOIs of works and their references
-alexandria3k --crossref-directory 'April 2022 Public Data File from Crossref'  \
-  --populate-db-path 5y.db --debug progress \
+$TIME alexandria3k --crossref-directory "$CROSSREF_DIR"  \
+  --populate-db-path "$DB_FILE" --debug progress \
   --columns works.doi works.issn_print works.issn_electronic \
     work_references.doi \
   --row-selection 'works.published_year BETWEEN 2017 AND 2021'
 
 # Add journal names
-alexandria3k --journal-names --populate-db-path 5y.db
+$TIME alexandria3k --journal-names --populate-db-path "$DB_FILE"
 
 # Calculate journal h5-index
-sqlite3 5y.db <journal-h5.sql
+$TIME sqlite3 "$DB_FILE" <$SQL_SCRIPT
