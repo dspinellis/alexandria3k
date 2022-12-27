0517af3a391948346357a26ad3df59c0caea1212
Diomidis Spinellis <dds@aueb.gr>
2022-12-27 12:57:42 +0200

    Take into account non-matched affiliations

--- examples/covid/work-affiliations.sql
+++ examples/covid/work-affiliations.sql
@@ -1,5 +1,6 @@
 -- Affiliations associated with works on COVID research
 
+-- Match works with identified authors' affiliations
 WITH work_rors AS (
   -- Works and participating RORs
   SELECT DISTINCT work_id, ror_id
@@ -7,14 +8,44 @@ WITH work_rors AS (
   LEFT JOIN work_authors
     ON work_authors_rors.work_author_id = work_authors.id
 ),
+
+-- Count works by research organization (ROR)
 ror_work_counts AS (
   SELECT ror_id, Count(*) AS number FROM work_rors GROUP BY ror_id
 ),
+
+-- Add ROR names
 ror_name_work_counts AS (
-  SELECT * from ror_work_counts
+  SELECT name, number from ror_work_counts
   INNER JOIN research_organizations
     ON ror_work_counts.ror_id = research_organizations.id
+),
+
+-- Match works with unidentified author affiliations
+unmatched_work_affiliations AS (
+  SELECT DISTINCT work_id, author_affiliations.name FROM
+    work_authors
+    INNER JOIN author_affiliations
+      ON work_authors.id = author_affiliations.author_id
+    LEFT JOIN work_authors_rors
+      ON work_authors_rors.work_author_id = work_authors.id
+    WHERE work_authors_rors.ror_id is null
+),
+
+-- Count works by unidentified author affiliations
+unmatched_affiliation_work_counts AS (
+  SELECT name, Count(*) AS number FROM unmatched_work_affiliations
+  GROUP BY name
+),
+
+-- Merge the two groups together
+all_work_counts AS (
+  SELECT * FROM ror_name_work_counts
+  UNION
+  SELECT * FROM unmatched_affiliation_work_counts
 )
+
+-- Output the top-20 affiliations according to number of published works
 SELECT Rank() OVER (ORDER BY number DESC) AS rank, number, name
-FROM ror_name_work_counts
+FROM all_work_counts
 LIMIT 20;
