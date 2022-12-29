3f44fc9e2b8dac1fda9ad12422f9f679828b2521
Diomidis Spinellis <dds@aueb.gr>
2022-12-29 17:01:04 +0200

    Calculate work subject citations

---
+++ examples/graph/work_subject_citations.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS work_subject_citations;
+
+CREATE TABLE work_subject_citations AS
+  SELECT
+      citing_subjects.asjc_id AS citing_subject_id,
+      cited_subjects.asjc_id AS cited_subject_id,
+      Count(*) AS citations_number
+    FROM works AS citing_works
+    INNER JOIN work_references ON citing_works.id = work_references.work_id
+    INNER JOIN works AS cited_works ON cited_works.doi = work_references.doi
+    INNER JOIN works_asjcs AS citing_subjects
+      ON citing_subjects.work_id = citing_works.id
+    INNER JOIN works_asjcs AS cited_subjects
+      ON cited_subjects.work_id = cited_works.id
+    GROUP BY citing_subject_id, cited_subject_id;
+
