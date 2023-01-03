794a0e6decc3f216108f4775ef82d0c883123dd5
Diomidis Spinellis <dds@aueb.gr>
2023-01-03 10:19:26 +0200

    Calculate additional Crossref metrics

---
+++ examples/graph/crossref-metrics.sql
@@ -0,0 +1,14 @@
+-- Obtain metrics not available from the graph
+WITH counts AS (
+  -- Get all measures in a single pass
+  SELECT type, abstract is not null AS have_abstract,
+    Count(*) AS number
+  FROM   works
+  GROUP by type, have_abstract
+)
+-- Disaggregate them
+SELECT 'type' as metric, type AS name, Sum(number)
+  FROM counts GROUP BY type
+UNION
+SELECT 'abstract' as metric, have_abstract AS name, Sum(number)
+  FROM counts GROUP BY have_abstract;
