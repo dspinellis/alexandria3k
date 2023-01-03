da80383ab8da19bd6cf90a63e507be22c11f6542
Diomidis Spinellis <dds@aueb.gr>
2023-01-03 12:07:11 +0200

    Add person h5-index calculation

---
+++ examples/person-h5/orcid_h5.sql
@@ -0,0 +1,20 @@
+-- Calculate h5-index for each ORCID
+CREATE INDEX IF NOT EXISTS rolap.works_orcid_doi_idx ON works_orcid(doi);
+CREATE INDEX IF NOT EXISTS rolap.works_orcid_orcid_idx ON works_orcid(orcid);
+CREATE INDEX IF NOT EXISTS rolap.work_citations_doi_idx ON work_citations(doi);
+
+CREATE TABLE IF NOT EXISTS rolap.orcid_h5 AS
+  WITH ranked_orcid_citations AS (
+    SELECT orcid, citations_number,
+      Row_number() OVER (
+        PARTITION BY orcid ORDER BY citations_number DESC) AS row_rank
+    FROM rolap.work_citations
+    INNER JOIN rolap.works_orcid ON rolap.works_orcid.doi
+      = rolap.work_citations.doi
+  ),
+  eligible_ranks AS (
+    SELECT orcid, row_rank FROM ranked_orcid_citations
+    WHERE row_rank <= citations_number
+  )
+  SELECT orcid, Max(row_rank) AS h5_index FROM eligible_ranks
+  GROUP BY orcid;
---
+++ examples/person-h5/works_orcid.sql
@@ -0,0 +1,10 @@
+-- ORCIDs associated with each work
+
+CREATE INDEX IF NOT EXISTS works_id_idx ON works(id);
+CREATE INDEX IF NOT EXISTS work_authors_work_id_idx ON work_authors(work_id);
+
+CREATE TABLE IF NOT EXISTS rolap.works_orcid AS
+  SELECT doi, orcid
+  FROM works
+  INNER JOIN work_authors ON works.id = work_authors.work_id
+  WHERE orcid is not null;
