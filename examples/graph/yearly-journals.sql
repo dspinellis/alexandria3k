57c97025839e9f90ed1375675f5e361f71511a6e
Diomidis Spinellis <dds@aueb.gr>
2023-01-17 22:59:43 +0200

    Test and fix yearly journals

--- examples/graph/yearly-journals.sql
+++ examples/graph/yearly-journals.sql
@@ -5,8 +5,13 @@ WITH
     FROM works
     WHERE published_year BETWEEN 1950 and 2021
       AND issn is not null
+  ),
+  issns_by_year AS (
+    SELECT published_year, issn
+    FROM issns
+    GROUP BY published_year, issn
   )
-SELECT published_year, Count(issn)
-FROM issns
+SELECT published_year, Count(*) AS n
+FROM issns_by_year
 GROUP BY published_year
 ORDER BY published_year;
