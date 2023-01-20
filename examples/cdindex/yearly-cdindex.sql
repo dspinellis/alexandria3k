f1af1fea9f3c8b602cc505f9abbfe77c43eb12a1
Diomidis Spinellis <dds@aueb.gr>
2023-01-20 20:29:40 +0200

    Use same range as for other evolution data

--- examples/cdindex/yearly-cdindex.sql
+++ examples/cdindex/yearly-cdindex.sql
@@ -2,5 +2,5 @@
 SELECT Strftime('%Y', datetime(timestamp, 'unixepoch')) AS year,
     Avg(cdindex) AS cdindex
   FROM rolap.cdindex
-  WHERE cdindex is not null
+  WHERE cdindex is not null AND year >= 1950
   GROUP BY year;
