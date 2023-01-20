a600ca331f6f2738982cd9cc9729e876a82096ee
Diomidis Spinellis <dds@aueb.gr>
2023-01-20 19:31:17 +0200

    Fix handling of cdindex timestamps

--- examples/cdindex/yearly-cdindex.sql
+++ examples/cdindex/yearly-cdindex.sql
@@ -1,5 +1,6 @@
 -- Calculate average CD-5 index per year
-SELECT Strftime('%Y', timestamp) AS year, Avg(cdindex) AS cdindex
+SELECT Strftime('%Y', datetime(timestamp, 'unixepoch')) AS year,
+    Avg(cdindex) AS cdindex
   FROM rolap.cdindex
   WHERE cdindex is not null
   GROUP BY year;
