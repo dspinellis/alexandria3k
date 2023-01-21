615504d348e410dc4cdbc1aac6ec01d693d2912b
Diomidis Spinellis <dds@aueb.gr>
2023-01-21 20:14:12 +0200

    Track work pages from 1945 onward

--- examples/yearly-numpages/works_pages.sql
+++ examples/yearly-numpages/works_pages.sql
@@ -6,7 +6,7 @@ CREATE TABLE rolap.works_pages AS
       Cast(Substr(page, Instr(page, "-") + 1) AS int) AS last_page,
       Cast(Substr(page, 0, Instr(page, "-")) AS int) AS first_page
     FROM works
-    WHERE published_year BETWEEN 1950 and 2021
+    WHERE published_year BETWEEN 1945 and 2021
   )
   SELECT published_year AS year, page, last_page - first_page + 1 AS num_pages
   FROM page_data
