fecbd827a96e2f37f1451a646f5a07ccba1d3271
Diomidis Spinellis <dds@aueb.gr>
2023-01-28 11:19:05 +0200

    Optimize work transfer: index, shared vertices

--- examples/cdindex/cdindex-db.py
+++ examples/cdindex/cdindex-db.py
@@ -20,6 +20,11 @@ DELTA = int(datetime.timedelta(days=365 * 5).total_seconds())
 
 BATCH_SIZE = 10000
 
+# Set to true to create a random population
+random_population = False
+RANDOM_POPULATION_SIZE = 1000000
+random.seed("xyzzy")
+
 RANGE = "published_year BETWEEN 1945 and 2021"
 
 debug.set_flags(["perf"])
@@ -40,13 +45,16 @@ def progress_output(phase, counter):
     print(f"{timestamp} {phase} {counter}", file=sys.stderr, flush=True)
 
 
-def add_random_vertices(db, graph):
-    for i in range(1, 100000):
-        graph.add_vertex(i, i)
+def add_random_vertices(graph):
+    """Add random vertices for testing"""
+    for i in range(1, RANDOM_POPULATION_SIZE):
+        dt = datetime.datetime(random.randint(1945, 2021), 1, 1)
+        graph.add_vertex(i, timestamp_from_datetime(dt))
     return
 
 
 def add_vertices(db, graph):
+    """Add works as vertices"""
     counter = 0
     for (doi, year, month, day) in db.execute(
         f"""
@@ -63,13 +71,15 @@ def add_vertices(db, graph):
     perf.log("Create vertices")
 
 
-def add_random_edges(db, graph):
-    for i in range(1, 500000):
-        graph.add_edge(random.randint(1, 100000 - 1), random.randint(1, 100000 - 1))
+def add_random_edges(graph):
+    """Add random edges for testing"""
+    for i in range(1, RANDOM_POPULATION_SIZE * 20):
+        graph.add_edge(random.randint(1, RANDOM_POPULATION_SIZE - 1), random.randint(1, RANDOM_POPULATION_SIZE - 1))
     return
 
 
 def add_edges(db, graph):
+    """Add the references as edges"""
     counter = 0
     for (source_doi, target_doi) in db.execute(
         f"""
@@ -91,24 +101,33 @@ def add_edges(db, graph):
     perf.log("Create edges")
 
 
-def process_batch(doi_list):
+def process_batch(start):
     results = []
-    for doi in doi_list:
+    for doi in vertices_list[start:start + BATCH_SIZE]:
         results.append((doi, graph.timestamp(doi), graph.cdindex(doi, DELTA)))
     return results
 
 
 if __name__ == '__main__':
-    db = sqlite3.connect(sys.argv[1])
-    create_indexes(db)
+
     global graph
     graph = cdindex.Graph()
-    add_vertices(db, graph)
-    add_edges(db, graph)
-    db.close()
+    if random_population:
+        add_random_vertices(graph)
+        add_random_edges(graph)
+    else:
+        db = sqlite3.connect(sys.argv[1])
+        create_indexes(db)
+        add_vertices(db, graph)
+        add_edges(db, graph)
+        db.close()
 
     graph.prepare_for_searching()
     perf.log("Prepare graph for searching")
+
+    # Shared between instances as a subscriptable list
+    global vertices_list
+    vertices_list = list(graph.vertices())
 
     # Calculate and add to the database the CD5 index for all works in the graph
     db = sqlite3.connect(sys.argv[2], check_same_thread=False)
@@ -118,19 +137,21 @@ if __name__ == '__main__':
     counter = 0
     with concurrent.futures.ProcessPoolExecutor(mp_context=mp.get_context('fork')) as executor:
         futures = []
-        for chunk in chunked(graph.vertices(), BATCH_SIZE):
-            future = executor.submit(process_batch, chunk)
+        for start in range(0, len(vertices_list), BATCH_SIZE):
+            future = executor.submit(process_batch, start)
             futures += [future]
             progress_output("S", counter);
             counter += 1
     perf.log("Submit work")
 
     counter = 0
+    cursor = db.cursor()
     for future in concurrent.futures.as_completed(futures):
         results = future.result()
-        db.executemany("INSERT INTO cdindex VALUES(?, ?, ?)", results)
+        cursor.executemany("INSERT INTO cdindex VALUES(?, ?, ?)", results)
         progress_output("R", counter);
         counter += 1
     perf.log("Calculate CD index")
 
+    db.commit()
     db.close()
