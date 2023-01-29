bc3ecbf021d4ea66dbd6042c3c118d0798a3996c
Diomidis Spinellis <dds@aueb.gr>
2023-01-29 10:56:20 +0200

    Update for missing timestamp column

--- examples/cdindex/yearly-cdindex.sql
+++ examples/cdindex/yearly-cdindex.sql
@@ -1,8 +1,12 @@
 -- Calculate average CD-5 index per year
-SELECT Cast(Strftime('%Y', datetime(timestamp, 'unixepoch')) AS int) AS year,
-    Avg(cdindex) AS cdindex
+
+CREATE INDEX IF NOT EXISTS rolap.cdindex_doi_idx ON cdindex(doi);
+CREATE INDEX IF NOT EXISTS works_doi_idx ON works(doi);
+
+SELECT published_year AS year, Avg(cdindex) AS cdindex
   FROM rolap.cdindex
-  WHERE cdindex is not null
+  INNER JOIN works ON works.doi = cdindex.doi
+  WHERE cdindex is not null AND published_year is not null
   GROUP BY year
   -- Years to 2021 are needed for calculating CD5
   HAVING year <= 2016;
