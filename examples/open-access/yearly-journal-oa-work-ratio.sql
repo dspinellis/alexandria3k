c49a57a927a16afa3baa7b8610186947f43f81fd
Diomidis Spinellis <dds@aueb.gr>
2023-05-18 23:29:48 +0300

    Add open access journal papers example study

---
+++ examples/open-access/yearly-journal-oa-work-ratio.sql
@@ -0,0 +1,28 @@
+-- Number of open access journal papers per year
+
+CREATE INDEX IF NOT EXISTS works_id_idx on works(id);
+CREATE INDEX IF NOT EXISTS rolap.oa_works_work_id_idx on oa_works(work_id);
+
+WITH oa_journal_papers AS (
+  SELECT published_year, Count(*) AS n
+    FROM works
+    INNER JOIN rolap.oa_works ON works.id = oa_works.work_id
+    WHERE published_year between 1945 and 2021
+    GROUP BY published_year
+),
+non_oa_journal_papers AS (
+  SELECT published_year, Count(*) AS n
+    FROM works
+    LEFT JOIN rolap.oa_works ON works.id = oa_works.work_id
+    WHERE published_year between 1945 and 2021
+      AND oa_works.work_id is null
+      AND (works.issn_print is not null OR works.issn_electronic is not null)
+    GROUP BY published_year
+)
+SELECT non_oa_journal_papers.published_year,
+  non_oa_journal_papers.n AS non_oa_n,
+  oa_journal_papers.n AS oa_n,
+  Cast(oa_journal_papers.n AS float)
+    / (non_oa_journal_papers.n + oa_journal_papers.n) AS oa_ratio
+  FROM non_oa_journal_papers LEFT JOIN oa_journal_papers ON
+    non_oa_journal_papers.published_year = oa_journal_papers.published_year;
