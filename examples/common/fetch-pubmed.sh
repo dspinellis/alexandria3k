cdd09ba31cb8c5b25a19ec0ce2e0fcd8544ab9c7
Bas Verlooy <bas.verlooy@gmail.com>
2024-01-21 22:34:44 +0100

    Finalise python script for retrieving software packages

--- examples/common/fetch-pubmed.sh
+++ examples/common/fetch-pubmed.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 # downloads all MEDLINE/Pubmed citations in the annual baseline.
+mkdir pubmed-data
+cd pubmed-data
+
+# each year the file names change, so we need to get the current year
+year=$(date +'%y')
 
 for i in $(seq 1 972); do
-    fname="1"
     if ((i < 10)); then
-        fname="ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed23n000$i.xml.gz"
+        fname="ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed${year}n000$i.xml.gz"
     elif ((i < 100)); then
-        fname="ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed23n00$i.xml.gz"
-    elif ((i < 1000)); then
-        fname="ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed23n0$i.xml.gz"
+        fname="ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed${year}n00$i.xml.gz"
+    else
+        fname="ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/pubmed${year}n0$i.xml.gz"
     fi
-    echo $fname;
-    wget $fname;
-    sleep 2;
+    echo "Downloading: $fname"
+    wget $fname
+    sleep 2
 done
