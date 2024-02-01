4af06101d2ff8463836dd8bcd584ce0c4c83899e
Diomidis Spinellis <dds@aueb.gr>
2024-02-01 09:55:46 +0200

    Add PubMed fetching rule

---
+++ examples/common/fetch-pubmed.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Fetch baseline PubMed bibliographic data
+#
+
+# Fail on command errors and unset variables
+set -eu
+
+BASE=https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/
+
+mkdir pubmed-data
+cd pubmed-data
+
+# Obtain last baseline file from README.txt
+last=$(curl --silent https://ftp.ncbi.nlm.nih.gov/pubmed/baseline/README.txt |
+  sed -n '/pubmed24n0001/s/.*pubmed24n\([^.]*\)\.xml.*/\1/p')
+
+for n in $(seq 1 $last) ; do
+  file_name=$(printf 'pubmed24n%04d.xml.gz' $n)
+
+  curl --silent $BASE/$file_name >$file_name
+done
