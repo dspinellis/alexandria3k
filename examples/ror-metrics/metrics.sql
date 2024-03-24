54d5829976235699a800406b5ca6d38bd688d6a7
Diomidis Spinellis <dds@aueb.gr>
2024-03-24 22:08:40 +0200

    Add research organization registry metrics

---
+++ examples/ror-metrics/metrics.sql
@@ -0,0 +1,7 @@
+SELECT "Organizations" AS name, Count(*) FROM research_organizations
+UNION
+SELECT "Aliases" AS name,   Count(*) from ror_aliases
+UNION
+SELECT "Acronyms" AS name,   Count(*) from ror_acronyms
+UNION
+SELECT "Relationships" AS name,   Count(*) from ror_relationships;
