c14284d20226f1da6986b936d95696c7fd057e02
Diomidis Spinellis <dds@aueb.gr>
2025-11-26 00:21:35 +0200

    Exclude empty venues

--- examples/greek-pubs/top-venues.sql
+++ examples/greek-pubs/top-venues.sql
@@ -1,6 +1,7 @@
 -- Top ten publication venues
 -- Venue|Publications
 SELECT  container_title, Count(*) FROM works
-GROUP BY container_title
-ORDER BY Count(*) DESC
-LIMIT 10;
+  WHERE container_title IS NOT null
+  GROUP BY container_title
+  ORDER BY Count(*) DESC
+  LIMIT 10;
