65285c91ac65a3b2c15ef75f02d0ae95f1090a17
Diomidis Spinellis <dds@aueb.gr>
2025-11-27 20:30:06 +0200

    Reformat

--- examples/greek-pubs/types.sql
+++ examples/greek-pubs/types.sql
@@ -1,5 +1,6 @@
 -- Types of publications
 -- Type|Number of works
-SELECT  type, Count(*) FROM works
-GROUP BY type
-ORDER BY Count(*) DESC;
+SELECT  type, Count(*)
+  FROM works
+  GROUP BY type
+  ORDER BY Count(*) DESC;
