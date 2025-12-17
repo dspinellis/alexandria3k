69a6c09fb6c4e4c68d52a73449d735f7de060abb
Diomidis Spinellis <dds@aueb.gr>
2025-12-17 14:23:24 +0200

    Round reported values

--- examples/journal-impact/journal-impact-report.sql
+++ examples/journal-impact/journal-impact-report.sql
@@ -19,13 +19,13 @@ SELECT
     journal_names.doi,
     impact_factor2.citations_number AS citations_number2,
     impact_factor2.publications_number AS publications_number2,
-    impact_factor2.impact_factor AS impact_factor2,
+    Round(impact_factor2.impact_factor, 2) AS impact_factor2,
     impact_factor5.citations_number AS citations_number5,
     impact_factor5.publications_number AS publications_number5,
-    impact_factor5.impact_factor AS impact_factor5,
+    Round(impact_factor5.impact_factor, 2) AS impact_factor5,
     journal_h5.h5_index,
     journal_h5.h5_median,
-    eigenfactor.eigenfactor_score
+    Round(eigenfactor.eigenfactor_score, 5)
   FROM journal_names
   INNER JOIN rolap.active_journals
     ON active_journals.id =  journal_names.id
