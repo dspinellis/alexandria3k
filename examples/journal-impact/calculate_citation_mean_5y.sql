280d37d61f600d55bdb97875924f8fdf5271b242
Panagiotis Spanakis <t8200158@aueb.gr>
2026-03-01 17:17:02 +0200

    journal-impact: Add SJR, AIS, SNIP metrics and network-based metrics

--- examples/journal-impact/calculate_citation_mean_5y.sql
+++ examples/journal-impact/calculate_citation_mean_5y.sql
@@ -1,10 +1,11 @@
--- Calculate the journal impact factor
+-- Calculate the Five-Year Citation Mean
+-- Note: This metric is similar to the 5-Year Impact Factor
 
-CREATE TABLE rolap.impact_factor5 AS
+CREATE TABLE rolap.citation_mean_5y AS
   SELECT publications5.journal_id,
     Coalesce(citations_number, 0) AS citations_number, publications_number,
     Cast(Coalesce(citations_number, 0) AS FLOAT) / publications_number
-      AS impact_factor
+      AS citation_mean
   FROM rolap.publications5
   LEFT JOIN rolap.citations5
     ON rolap.citations5.journal_id = rolap.publications5.journal_id
