3be04c9282b875b5f85168246ca754395aa8580e
kostisTses <kostis.tsesmelis@gmail.com>
2026-07-02 23:33:42 +0300

    added unit tests

---
+++ tests/processes/test_link_and_table.py
@@ -0,0 +1,104 @@
+#
+# Alexandria3k Crossref bibliographic metadata processing
+# Copyright (C) 2026  Diomidis Spinellis
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""link_and_table scoring function unit tests"""
+
+import unittest
+
+from ..test_dir import add_src_dir
+
+add_src_dir()
+
+from alexandria3k.processes.link_and_table import (
+    jaccard_similarity,
+    jaro_distance,
+    jaro_Winkler,
+)
+from alexandria3k.processes.link_author_blocks import normalized
+
+
+class TestJaroDistance(unittest.TestCase):
+    def test_identical_strings(self):
+        self.assertEqual(jaro_distance("same", "same"), 1.0)
+
+    def test_disjoint_strings(self):
+        self.assertEqual(jaro_distance("abc", "xyz"), 0.0)
+
+    def test_known_reference_pair(self):
+        # Standard textbook example (Winkler, 1990)
+        self.assertAlmostEqual(jaro_distance("martha", "marhta"), 0.9444444444444445)
+
+    def test_one_empty_string(self):
+        self.assertEqual(jaro_distance("", "abc"), 0.0)
+
+    def test_both_empty_strings(self):
+        self.assertEqual(jaro_distance("", ""), 1.0)
+
+
+class TestJaroWinkler(unittest.TestCase):
+    def test_identical_strings(self):
+        self.assertEqual(jaro_Winkler("same", "same"), 1.0)
+
+    def test_known_reference_pair(self):
+        # Jaro-Winkler boosts the base Jaro score for a shared prefix
+        self.assertAlmostEqual(jaro_Winkler("martha", "marhta"), 0.9611111111111111)
+
+    def test_boost_requires_jaro_above_threshold(self):
+        # Below the 0.7 Jaro threshold, no prefix boost is applied
+        base = jaro_distance("abc", "xyz")
+        self.assertEqual(jaro_Winkler("abc", "xyz"), base)
+
+
+class TestJaccardSimilarity(unittest.TestCase):
+    def test_identical_sets(self):
+        self.assertEqual(jaccard_similarity({1, 2, 3}, {1, 2, 3}), 1.0)
+
+    def test_disjoint_sets(self):
+        self.assertEqual(jaccard_similarity({1, 2}, {3, 4}), 0.0)
+
+    def test_partial_overlap(self):
+        self.assertEqual(jaccard_similarity({1, 2, 3}, {2, 3, 4}), 0.5)
+
+    def test_both_sets_empty(self):
+        self.assertEqual(jaccard_similarity(set(), set()), 0)
+
+    def test_one_set_empty(self):
+        self.assertEqual(jaccard_similarity({1, 2}, set()), 0)
+
+
+class TestNormalized(unittest.TestCase):
+    def test_lowercases(self):
+        self.assertEqual(normalized("JOHN"), "john")
+
+    def test_strips_decomposable_diacritics(self):
+        # NFKD decomposes accented letters into base + combining mark,
+        # which is then stripped
+        self.assertEqual(normalized("José"), "jose")
+
+    def test_strips_punctuation_and_digits(self):
+        self.assertEqual(normalized("O'Brien-123"), "obrien")
+
+    def test_keeps_internal_spaces(self):
+        self.assertEqual(normalized(" Jean Paul "), "jean paul")
+
+    def test_stroke_letters_are_not_folded(self):
+        self.assertEqual(normalized("Øystein"), "øystein")
+
+
+if __name__ == "__main__":
+    unittest.main()
